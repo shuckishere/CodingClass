16afa20866dae803afcb6b3af082ecad73a0323f
Gavin Martin <gavinashermartin@gmail.com>
2022-06-14 13:09:30 -0400

    Update snake.py
    Added boundries and game over message

--- snake.py
+++ snake.py
@@ -1,25 +1,78 @@
-from turtle import Turtle, Screen
+import turtle
 
-sreen = Screen()
-sreen.bgcolor('lightblue')
+screen = turtle.Screen()
+screen.bgcolor('black')
+#screen.screensize(3500, 350)
 
-snake = Turtle()
+
+snake = turtle.Turtle()
 snake.color('red')
 snake.penup()
 
-speed = 5
+Boundry = 250
+bigBondry = 500
+
+speed = 3
+
+
+
+
+def inBounds():
+    position = snake.pos()
+    #print(position[0])
+    if position[0] > Boundry or position[0] < -Boundry or position[1] > Boundry or  position[1] < -Boundry: 
+        snake.hideturtle()
+        endGame()
+        return False
+    else:
+        return True
 
 def travel():
-    snake.forward(speed)
-    sreen.ontimer(travel, 10)
+    isItGood = inBounds()
+    if isItGood == False:
+        return False
+    else:
+        snake.forward(speed)
+        screen.ontimer(travel, 10)
+    
 
-sreen.onkey(lambda: snake.setheading(90), 'Up')
-sreen.onkey(lambda: snake.setheading(180), 'Left')
-sreen.onkey(lambda: snake.setheading(0), 'Right')
-sreen.onkey(lambda: snake.setheading(270), 'Down')
 
-sreen.listen()
+def drawBoundry():
+    wall = turtle.Turtle()
+    wall.penup()
+    wall.speed(1000000)
+    wall.fillcolor("light green")
+    wall.begin_fill()
+    wall.goto(Boundry, Boundry)
+    wall.pendown()
+    wall.goto(-Boundry, Boundry)
+    wall.goto(-Boundry, -Boundry)
+    wall.goto(Boundry, -Boundry)
+    wall.goto(Boundry, Boundry)
+    wall.end_fill()
+    wall.hideturtle()
 
+def endGame():
+    penBoi = turtle.Turtle()
+    penBoi.hideturtle()
+    penBoi.speed(999999999999999999999999999999999999)
+    penBoi.penup()
+    penBoi.goto(0,200)
+    penBoi.color('red')
+    style = ('Courier', 30, 'italic')
+    penBoi.write('GAME OVER', font=style, align="center")
+    
+
+
+screen.onkey(lambda: snake.setheading(90), 'Up')
+screen.onkey(lambda: snake.setheading(180), 'Left')
+screen.onkey(lambda: snake.setheading(0), "Right")
+screen.onkey(lambda: snake.setheading(270), 'Down')
+
+screen.listen()
+
+
+drawBoundry()
 travel()
 
-sreen.mainloop()
+screen.mainloop()
