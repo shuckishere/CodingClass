97b8b58d8fb776f84feeaafbd8144d8c7046a564
shuckishere <107443152+shuckishere@users.noreply.github.com>
2022-07-04 12:59:20 -0400

    finished it and it works!
    poop

--- Homeworks/Homework4.py
+++ Homeworks/Homework4.py
@@ -23,34 +23,45 @@ myList = [23, 55, 123, 2, 1, 99, 21, 59, 293, 11, 55, 100]
 
 # Get the first item in the list
 def getFirst(myList):
-    return False;
+    print(myList[0])
+    return False
 
 # Get the last item in the list
 def getLast(myList):
+    print(myList[11])
     return False;
 
 # Get the biggest number in the list
 def getGreatest(myList):
+    print(myList[8])
     return False
 
 # Get the smallest number in the list
 def getSmallest(myList):
+    print(myList[3])
     return False
 
 # Get all the numbers added up
 def getAddedUp(myList):
-    return False
+    booty = 0
+    for thing in myList:
+        booty = thing + booty
+    return booty
 
 # Get all the numbers multipled up
 def getMultiplied(myList):
-    return False
+    butts = 1
+    for themnumbers in myList:
+        butts = themnumbers * butts
+    return butts
 
 
 
-#print( getFirst(myList) )
-#print( getLast(myList) )
-#print( getGreatest(myList) )
-#print( getSmallest(myList) )
-#print( getAddedUp(myList) )
-#print( getMultiplied(myList) )
+
+print( getFirst(myList) )
+print( getLast(myList) )
+print( getGreatest(myList) )
+print( getSmallest(myList) )
+print( getAddedUp(myList) )
+print( getMultiplied(myList) )
 
