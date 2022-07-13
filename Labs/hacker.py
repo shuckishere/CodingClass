d774401d546a88f6f3d031df3f2eb2de73d07060
Gavin Martin <gavinashermartin@gmail.com>
2022-07-13 18:39:21 -0400

    Create hacker.py
    code to start hacking

---
+++ Labs/hacker.py
@@ -0,0 +1,83 @@
+# ===== IMPORTS ======
+import turtle
+
+
+# ====== Contants ======
+screen = turtle.Screen()
+screen.bgcolor('green')
+
+# ======= Functions ========
+
+def writePasswordInitial(password):
+    passTurtle = turtle.Turtle()
+    passTurtle.hideturtle()
+    passTurtle.penup()
+    passTurtle.speed(999999999999999999)
+    passTurtle.goto(0,250)
+    style = ('Courier', 25, 'italic')
+    passTurtle.write(str("Your Password: " + password), font=style, align="center")
+
+
+def setupGuessTurtle():
+    global guessTurtle
+    guessTurtle = turtle.Turtle()
+    guessTurtle.hideturtle()
+    guessTurtle.speed(999999999999999999)
+    guessTurtle.penup()
+    guessTurtle.goto(0,150)
+    
+
+def writeGuess(guess):
+    global guessTurtle
+    global password
+
+    print(guess)
+    guessTurtle.clear()
+    style = ('Courier', 30, 'italic')
+    guessTurtle.write(str(guess), font=style, align="center")
+
+
+def hackPassword(showUpdates):
+    global password
+    passwordGuess = ""
+    attempts = 0
+    while(password != passwordGuess):
+        print(passwordGuess)
+
+
+
+
+
+
+
+
+
+        if(showUpdates == True):
+            writeGuess(passwordGuess)
+        attempts = attempts + 1
+    writeGuess(passwordGuess)
+    print("Hacked your password in " + str(attempts) + " attempts")
+
+
+
+# ======= Main ============
+password = turtle.textinput("Hacker Maker", "Enter your password")
+
+writePasswordInitial(password)
+setupGuessTurtle()
+
+hackPassword(False)
+
+
+
+print("\n\n\n")
+
+
+
+screen.mainloop()
+
+
+
+
+
+
